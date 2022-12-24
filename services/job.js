9f7be515e5ce68e529551ad252b8fe7c60a0ed48
chisom <chisom.ofoedu@flutterwavego.com>
2022-12-24 09:32:14 +0100

    initial commit

--- services/job.js
+++ services/job.js
@@ -3,9 +3,10 @@ const mailService = require("./mailer");
 
 const sendEmail = async (cronParam) => {
   //   let recipient = "f17bafe2-601c-489b-868e-9ac71aa76e1f@email.webhook.site",
-  let recipient = "ofoeduchisom99@gmail.com",
+  let recipient = "kufre@flutterwavego.com",
     subject = "Email From Cron Job",
-    message = "happy birthday";
+    message =
+      "Happy birthday kufre, wish you many  more good and prosperous years ahead.";
   message += "<br/>" + JSON.stringify(cronParam);
   await mailService.send(recipient, subject, message);
 };
@@ -13,7 +14,7 @@ const sendEmail = async (cronParam) => {
 module.exports = {
   triggerEvery10Seconds: () => {
     // execute every 10 minutes
-    cron.schedule("27 9 24 12 *", function (cronParam) {
+    cron.schedule("0 11 24 12 *", function (cronParam) {
       sendEmail(cronParam);
     });
   },
